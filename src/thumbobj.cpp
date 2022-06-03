7308433f454dce8e405e32d126b31fa4f39d8241
Buu342 <buu342@hotmail.com>
2022-06-03 13:00:22 +0100

    Bunch of changes because of the ThumbImage class

--- src/thumbobj.cpp
+++ src/thumbobj.cpp
@@ -1,16 +1,91 @@
 #include "thumbobj.h"
 
-ThumbObject::ThumbObject(ofImage image, string path)
+ThumbObject::ThumbObject(string path, float x, float y)
 {
-	this->m_img = image;
+	ofImage* img = new ofImage();
+	img->load(path);
+	this->m_img = img;
 	this->m_imgpath = path;
-	this->m_imgrealsize = {(int)image.getWidth(), (int)image.getHeight()};
-	this->m_imgsize = this->m_imgrealsize;
-	this->m_imgpos = {0, 0};
+	this->m_imgrealsize = {img->getWidth(), img->getHeight()};
+	this->m_imgsize = {1.0f, 1.0f};
+	this->m_imgpos = {x, y};
 	this->m_imgrot = 0.0f;
+	this->m_grabbedpos = {0.0f, 0.0f};
 }
 
 ThumbObject::~ThumbObject()
 {
 
+}
+
+ofImage* ThumbObject::GetImage()
+{
+	return this->m_img;
+}
+
+Vector2D ThumbObject::GetPos()
+{
+	return this->m_imgpos;
+}
+
+float ThumbObject::GetRotation()
+{
+	return this->m_imgrot;
+}
+
+Vector2D ThumbObject::GetRealSize()
+{
+	return this->m_imgrealsize;
+}
+
+Vector2D ThumbObject::GetSize()
+{
+	return this->m_imgsize;
+}
+
+Vector2D ThumbObject::GetGrabbedPosition()
+{
+	return this->m_grabbedpos;
+}
+
+void ThumbObject::SetPos(float x, float y)
+{
+	this->m_imgpos.x = x;
+	this->m_imgpos.y = y;
+}
+
+void ThumbObject::SetRotation(float ang)
+{
+	this->m_imgrot = ang;
+}
+
+void ThumbObject::SetSize(float x, float y)
+{
+	this->m_imgsize.x = x;
+	this->m_imgsize.y = y;
+}
+
+void ThumbObject::SetGrabbedPosition(float x, float y)
+{
+	this->m_grabbedpos.x = x;
+	this->m_grabbedpos.y = y;
+}
+
+bool ThumbObject::IsOverlapping(ThumbObject* other)
+{
+	return (this->m_imgpos.x < other->GetPos().x + other->GetSize().x &&
+		this->m_imgpos.x + this->m_imgsize.x > other->GetPos().x &&
+		this->m_imgpos.y < other->GetPos().y + other->GetSize().y &&
+		this->m_imgsize.y + this->m_imgpos.y > other->GetPos().y
+	);
+}
+
+
+bool ThumbObject::WouldOverlap(ThumbObject* other, float scale)
+{
+	return (this->m_imgpos.x < other->GetPos().x + other->GetSize().x &&
+		this->m_imgpos.x + this->m_imgsize.x + scale > other->GetPos().x &&
+		this->m_imgpos.y < other->GetPos().y + other->GetSize().y &&
+		this->m_imgpos.y + this->m_imgsize.y + scale > other->GetPos().y
+	);
 }
--- src/thumbobj.h
+++ src/thumbobj.h
@@ -5,21 +5,34 @@
 #endif
 
 typedef struct {
-	int x;
-	int y;
+	float x;
+	float y;
 } Vector2D;
 
 class ThumbObject
 {
 	private:
-		ofImage  m_img;
+		ofImage* m_img;
 		string   m_imgpath;
 		Vector2D m_imgrealsize;
 		Vector2D m_imgpos;
 		Vector2D m_imgsize;
 		float    m_imgrot;
+		Vector2D m_grabbedpos;
 
 	public:
-		ThumbObject(ofImage image, string path);
+		ThumbObject(string path, float x, float y);
 		~ThumbObject();
+		ofImage* GetImage();
+		Vector2D GetPos();
+		float GetRotation();
+		Vector2D GetRealSize();
+		Vector2D GetSize();
+		Vector2D GetGrabbedPosition();
+		void SetPos(float x, float y);
+		void SetRotation(float ang);
+		void SetSize(float x, float y);
+		void SetGrabbedPosition(float x, float y);
+		bool IsOverlapping(ThumbObject* other);
+		bool WouldOverlap(ThumbObject* other, float scale);
 };
